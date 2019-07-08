8e319369202b28fe174489e237060c56f75e3cd1
Jack Greaney <jack.greaney@gmail.com>
2019-07-07 20:57:15 -0500

    Create collatz.c

---
+++ collatz.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int collatz(int n)
+{
+    if (n == 1)
+        return 0;
+    else if (n % 2 == 0 )
+        return 1 + collatz(n / 2);
+    else
+        return 1 + collatz((3 * n) + 1);        
+}
+
+int main(void)
+{
+    int n = get_int("Hello give me an int: ");
+    int k = collatz(n);
+    printf("Steps: %i\n", k);
+}
